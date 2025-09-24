975db5613425bc65537db53bb739f142f8fb5259
MasterDuos <rkevinnavid@gmail.com>
2025-09-24 05:08:03 -0500

    2 avance

--- blog/urls.py
+++ blog/urls.py
@@ -4,3 +4,11 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
 ]
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("post/<int:id>/", views.post_detalle, name="post_detalle"),  # 👈 nueva ruta
+]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
 def home(request):
-    return render(request, "blog/home.html")
+    posts = Post.objects.all().order_by('-fecha_creacion')
+    return render(request, "blog/home.html", {"posts": posts})
+
+from django.shortcuts import render, get_object_or_404
+
+def post_detalle(request, id):
+    post = get_object_or_404(Post, id=id)
+    return render(request, "blog/post_detalle.html", {"post": post})
