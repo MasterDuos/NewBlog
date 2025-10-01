0db258a7d6dedb29b9fb522a65fbdefbe462fd94
MasterDuos <rkevinnavid@gmail.com>
2025-10-01 16:07:17 -0500

    prueba 10

--- core/settings.py
+++ core/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-9hkj3m(0juyk+8%oqi5d#gj!tjge1#4=rf%bn-)fwwewv=^5m3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*.up.railway.app", "localhost", "127.0.0.1"]
 
 
 # Application definition
